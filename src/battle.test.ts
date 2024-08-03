1600030d7251d605bf7518c1794810cda5bef6d2
Sergio <sergio@garciadelacruz.es>
2024-08-03 20:06:09 +0200

    Fix crits

--- src/battle.test.ts
+++ src/battle.test.ts
@@ -17,27 +17,31 @@ async function test(move: string, p1:Pokemon, p2:Pokemon) {
 	const battle = commonTools.createBattle();
 	battle.resetRNG(null);
 	battle.setPlayer('p1', {team: [{species: p1.species, ability: p1.ability || 'Pressure', moves: [move]}]});
-	battle.setPlayer('p2', {team: [{species: p2.species, ability: p2.ability || 'Pressure', moves: ['sleeptalk']}]});
+	battle.setPlayer('p2', {team: [{species: p2.species, ability: p2.ability || 'Battle Armor', moves: ['sleeptalk']}]});
 	battle.makeChoices(`move ${move}`, 'move sleep talk');
 	const gen = Generations.get(battle.gen);
 	const result = calculate(
 		gen,
 		new Pokemon(gen, p1.species, {ability: p1.ability || 'Pressure'}),
-		new Pokemon(gen, p2.species, {ability: p2.ability || 'Pressure'}),
+		new Pokemon(gen, p2.species, {ability: p2.ability || 'Battle Armor'}), // Battle Armor is used to prevent crits
 		new Move(gen, move),
 	);
 	const damage = battle.p2.active[0].maxhp - battle.p2.active[0].hp;
+	if(battle.p2.active[0].maxhp < result.damage[0]) {
+		throw new Error(`This test is useless, the defender is dead. Attacker: ${p1.species}, Defender: ${p2.species}, Move: ${move}`);
+	}
 	if(Array.isArray(result.damage[0])) {
 		const damage_rolls = [...new Set(fastCartesian(result.damage as number[][]).map((d) => d.reduce((a, b) => a + b, 0)))];
-		assert(damage_rolls.includes(damage), 'Showdown damage is impossible according to damage-calc. Showdown: ' + damage + ', damage-calc: ' + damage_rolls);
+		assert(damage_rolls.includes(damage), `Showdown damage is impossible according to damage-calc. Attack: ${p1.species}, Defender: ${p2.species}, Move: ${move}. Showdown: ${damage}, damage-calc: ${result.damage}, seed: ${battle.prng.seed}, logs: ${battle.log.join('\n')}`);
 	}
 	else {
-		assert(result.damage.includes(damage), 'Showdown damage is impossible according to damage-calc. Showdown: ' + damage + ', damage-calc: ' + result.damage);
+		assert(result.damage.includes(damage), `Showdown damage is impossible according to damage-calc. Attack: ${p1.species}, Defender: ${p2.species}, Move: ${move}. Showdown: ${damage}, damage-calc: ${result.damage}, seed: ${battle.prng.seed}, logs: ${battle.log.join('\n')}`);
 	}
 }
 
+// TODO: If an ability is specified for the defender, the test can fail due to crits
 const tests: readonly [string, Pokemon, Pokemon][] = [
-	['psystrike', {species: 'Mewtwo-Mega-Y'}, {species: 'Blissey'}],
+	['psystrike', {species: 'Mewtwo-Mega-Y'}, {species: 'Kyogre'}],
 	['fake out', {species: 'Kangaskhan-Mega', ability: 'Parental Bond'}, {species: 'Shuckle'}],
 ] as const;
 
